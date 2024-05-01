65444792ec2196b0a9de6dfc6f5131b393e529a1
Patryk <146970940+minefunk@users.noreply.github.com>
2024-05-01 19:08:23 +0200

    Delete kalk4Object directory

--- kalk4Object/app/kalkForm.class.php
+++
@@ -1,6 +0,0 @@
-<?php
-class CalcForm {
-	public $wysokoscKredytu;
-	public $oprocentowanie;
-	public $lata;
-} 
--- kalk4Object/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once dirname(__FILE__).'/config.php';
-//przekierowanie przeglądarki klienta (redirect)
-//header("Location: ".$conf->root_path."/app/calc.php");
-
-//przekazanie żądania do następnego dokumentu ("forward")
-
-include $conf->root_path.'/app/kalkulatorKredytowy.php';
