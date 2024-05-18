d14700d9d3450866da3712dd0b402bc8ffa0db8f
Patryk <146970940+minefunk@users.noreply.github.com>
2024-05-18 16:49:51 +0200

    Update ctrl.php

--- php05_A/ctrl.php
+++ php05_A/ctrl.php
@@ -3,26 +3,26 @@ require_once 'init.php';
 
 
 switch ($action) {
-	default : // 'calcView'
-	    // załaduj definicję kontrolera
+	default : 
+	   
 		include_once 'app/controllers/CalcCtrl.class.php';
-		// utwórz obiekt i uzyj
+		
 		$ctrl = new CalcCtrl ();
 		$ctrl->generateView ();
 	break;
 	case 'calcCompute' :
-		// załaduj definicję kontrolera
+		
 		include_once 'app/controllers/CalcCtrl.class.php';
-		// utwórz obiekt i uzyj
+		
 		$ctrl = new CalcCtrl ();
 		$ctrl->process ();
 	break;
 	case 'action1' :
-		// zrób coś innego ...
+		
 		print('hello');
 	break;
 	case 'action2' :
-		// zrób coś innego ...
+		
 		print('goodbye');
 	break;
 }
